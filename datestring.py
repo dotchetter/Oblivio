3dac012a9d28f48cf869a658542c4ea67d95a7e9
dotchetter <dotchetter@protonmail.ch>
2018-12-24 21:59:34 +0100

    Class created to handle time objects separately. Diff computing method written to handle generating array with inactive CROS devices.

---
+++ datestring.py
@@ -0,0 +1,24 @@
+
+
+class Datestring():
+
+	def __init__(self, past, present):
+
+		self.past = get_then(__dateobject)
+		self.present = get_today(__dateobject)
+
+
+	def get_today(dateobject):
+	    ''' Return today's date as string, format yyyy-mm-dd '''	
+
+	    today = dateobject.strftime('%Y-%m-%d')
+	    if len(today) == 10:
+	    	return today
+
+	def get_then(dateobject):
+	    ''' Return a date for (today minus 10 days) as string, format yyyy-mm-dd '''	
+
+	    _tendaysago = dateobject + timedelta(days = ( - 50))
+	    tendaysago = _tendaysago.strftime('%Y-%m-%d')
+	    if len(tendaysago) == 10:
+	    	return tendaysago
