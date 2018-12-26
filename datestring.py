ff6a48ebebc43d2252eb8fae39d554b3a3a3f6a8
dotchetter <dotchetter@protonmail.ch>
2018-12-26 22:53:29 +0100

    Corrected __init__ argument

--- datestring.py
+++ datestring.py
@@ -2,8 +2,9 @@
 
 class Datestring():
 
-	def __init__(self, past, present):
+	def __init__(self, dateobject = None, past = None, present = None):
 
+		self.dateobject = dateobject
 		self.past = get_then(__dateobject)
 		self.present = get_today(__dateobject)
 
@@ -14,6 +15,7 @@ class Datestring():
 	    today = dateobject.strftime('%Y-%m-%d')
 	    if len(today) == 10:
 	    	return today
+
 
 	def get_then(dateobject):
 	    ''' Return a date for (today minus 10 days) as string, format yyyy-mm-dd '''	
