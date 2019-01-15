cb0320a79fb64b18a2ae4de000b23168d4fea8c4
dotchetter <dotchetter@protonmail.ch>
2019-01-15 17:24:38 +0100

    Error handling function written, compute_diff() now returns inactive devices with only date and serial number (minus the deviceID).

--- main.py
+++ main.py
@@ -1,61 +1,90 @@
-
-from datestring import Datestring
+import sys
 import subprocess
-
+from datestring import Datestring
 
 def main():
-    # Create date object with today's date (self.present) and 10 days ago (self.past)
+    # Verify platform compatibility
+    assert (
+        'win32' in sys.platform
+    ), err_handler(exception_type = 'Exception', task = 'platform')
+    
+    # Date object with today's date (self.present) and 10 days ago (self.past)
     dateobj = Datestring()
+    
     # Call functions to fetch lists of chrome devices from G Suite with GAM
     active_crosdev = get_cros(dateobj.present, dateobj.past, domain_wide = False)
     all_crosdev = get_cros(dateobj.present, dateobj.past, domain_wide = True)
-    # Calculate the inactive devices by subtracting the active from all
+
+    # Calculate the inactive devices by subtracting the active ones
     inactive_crosdev = compute_diff(active_crosdev, all_crosdev)
+    for i in inactive_crosdev:
+        print(i)
+
+
+def get_cros(today, then, domain_wide = False):
+    ''' Call GAM and fetch Chrome OS devices from the domain. domain_wide
+    or not will determine if all chrome os devices are fetched or only the active
+    ones in the given time frame. '''
+
+    if domain_wide == True:
+        gam_command = (
+            'gam print cros orderby lastsync fields lastsync, serialnumber'
+        )
+    else:
+        gam_command = (
+            'gam print cros query sync:' + then + '..' + today 
+            + ' fields lastsync, serialnumber orderby lastsync serialnumber'
+        )
+    
+    # Call GAM and run command depending on the query (domain wide or not)
+    try:
+        gam_call = subprocess.Popen(
+            gam_command, stdout = subprocess.PIPE, shell = False
+        )
+        gam_output = str(gam_call.communicate()).split('\\r\\n')
+
+    except:
+        err_handler(exception_type = RuntimeError, task = 'gam')
+
+    else:
+        # Create a list containing only the devices, not header in the response
+        devices_arr = [
+            i for i in gam_output if not 'None' in i and not 'deviceId' in i
+        ]
+        
+        return devices_arr
 
 
 def compute_diff(active_devices, all_devices):
-    ''' Fetch devices from G Suite and compare active devices in G suite versus 
-    all devices to compute inactive devices within given time scope '''
-    inactive_devices = [i for i in all_devices if not i in active_devices]
+    ''' Calculate the inactive devices in the given time frame.
+    List is made containing only the devices not occurring in both all and active
+    devices. This yields the devices not used in the given time frame. '''
+    inactive_devices = [
+        i for i in all_devices if not i in active_devices
+    ]
     
-    # DEBUG:
-    print(len(inactive_devices), 'inactive devices found...')
-    print(inactive_devices)
-
-def get_cros(today, then, domain_wide = False):
-    ''' Ask GAM to fetch all cros devices in to set() and return it.
-    TODO: Date time variables for command towards GAM. Arguments tell whether
-    to do a domain-wide query or to fetch only active. today, then, represent
-    date strings for GAM query command. '''
-
-    if domain_wide == True:
-        gam_command = ('gam print cros orderby lastsync '
-                        'fields lastsync, serialnumber')
+    # Keep only last sync date and serialnumber for each index in the list
+    if len(inactive_devices):
+        for i in range(len(inactive_devices)):
+            inactive_devices[i] = inactive_devices[i].split(',')
+            inactive_devices[i] = inactive_devices[i][1:]
+            #print(device)
+        return inactive_devices
     else:
-        gam_command = ("gam print cros query sync:" + then + '..' + today 
-                        + " fields lastsync, serialnumber orderby lastsync serialnumber")
+        return(None)
+   
+
+def err_handler(exception_type = None, task = None):
+    ''' Handle errors on exception and stop execution '''
     
-    # Call GAM and run command depending on the query (domain wide or not)
-    gam_call = subprocess.Popen(gam_command, stdout=subprocess.PIPE)
-    devices = str(gam_call.communicate()).split('\\r\\n')
+    if task == 'gam':
+        msg = 'Could not proceed with the task; GAM is not responding.\n'
+    elif task == 'platform':
+        msg = 'This code is currently designed to run on Windows only.'
 
-    # Create a list containing only the devices, not header in the query
-    devices_arr = [i for i in devices if not 'None' in i and not 'deviceId' in i]
-    
-    if len(devices_arr):
-        for i in devices_arr:
-            i.split()
-        return devices_arr
-    else:
-        # Move up
-        return('No active devices between ' + str(then) + ' and ' + str(today))
+    raise exception_type(msg)
+    sys.exit()
 
-
-
-def err_handler(Error = None):
-    pass
-
-# debug: 
-
+ 
 if __name__ == '__main__':
     main()
