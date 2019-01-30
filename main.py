f3d9424653e1ba941219058654b9297e0496601b
dotchetter <dotchetter@dotchetters-MacBook-Pro.local>
2019-01-30 13:52:54 +0100

    Branch for MacOS with kernel-specific syntax when GAM is called upon. This presumes that GAM is installed in the default directory.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import os
 import sys
 import subprocess
 from datestring import Datestring
 
+
 def main():
     # Verify platform compatibility
     assert (
-        'win32' in sys.platform
+        'darwin' in sys.platform
     ), err_handler(exception_type = 'Exception', task = 'platform')
     
     # Date object with today's date (self.present) and 10 days ago (self.past)
@@ -17,8 +19,11 @@ def main():
 
     # Calculate the inactive devices by subtracting the active ones
     inactive_crosdev = compute_diff(active_crosdev, all_crosdev)
-    for i in inactive_crosdev:
-        print(i)
+    
+    if inactive_crosdev != None:
+        print('\n', 'Oblivio found ', len(inactive_crosdev), 'inactive devices:', end = '\n')
+        for i in inactive_crosdev:
+            print(i, sep = '\n')
 
 
 def get_cros(today, then, domain_wide = False):
@@ -28,31 +33,31 @@ def get_cros(today, then, domain_wide = False):
 
     if domain_wide == True:
         gam_command = (
-            'gam print cros orderby lastsync fields lastsync, serialnumber'
+            '~/bin/gam/gam print cros orderby lastsync fields lastsync, serialnumber'
         )
     else:
         gam_command = (
-            'gam print cros query sync:' + then + '..' + today 
+            '~/bin/gam/gam print cros query sync:' + then + '..' + today 
             + ' fields lastsync, serialnumber orderby lastsync serialnumber'
         )
     
     # Call GAM and run command depending on the query (domain wide or not)
     try:
         gam_call = subprocess.Popen(
-            gam_command, stdout = subprocess.PIPE, shell = False
+            gam_command, stdout = subprocess.PIPE, shell = True
         )
-        gam_output = str(gam_call.communicate()).split('\\r\\n')
+        _gam_output = str(gam_call.communicate()).split('\\')
 
     except:
         err_handler(exception_type = RuntimeError, task = 'gam')
 
     else:
-        # Create a list containing only the devices, not header in the response
-        devices_arr = [
-            i for i in gam_output if not 'None' in i and not 'deviceId' in i
+        # Comprehend a list containing only the devices, not header in the response
+        _device_list = [
+            i for i in _gam_output if not 'None' in i and not 'deviceId' in i
         ]
-        
-        return devices_arr
+
+        return _device_list
 
 
 def compute_diff(active_devices, all_devices):
@@ -71,16 +76,16 @@ def compute_diff(active_devices, all_devices):
             #print(device)
         return inactive_devices
     else:
-        return(None)
+        return None
    
 
 def err_handler(exception_type = None, task = None):
     ''' Handle errors on exception and stop execution '''
     
     if task == 'gam':
-        msg = 'Could not proceed with the task; GAM is not responding.\n'
+        msg = 'Could not proceed; GAM is not responding, is it installed?\n'
     elif task == 'platform':
-        msg = 'This code is currently designed to run on Windows only.'
+        msg = 'This version of Oblivio is designed to run on MacOS only.'
 
     raise exception_type(msg)
     sys.exit()
