aea42eb83a978decbdae3b60986c453c12e9b400
dotchetter <dotchetter@protonmail.ch>
2019-02-05 10:48:20 +0100

    bug fix: sys.platform changed to platform

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
     
     # Verify platform compatibility
     assert (
-        'darwin' in sys.platform
+        'darwin' in platform
     ), err_handler(exception_type = 'Exception', task = 'platform')
     
     # Check that GAM resides in the presumed directory
