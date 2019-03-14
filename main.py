6c2a30e545fb56173d883d2c59e70a99d5b635bf
dotchetter <dotchetter@protonmail.ch>
2019-03-14 09:13:04 +0100

    Major  commit;
    main module revamped to instanciate inventory() object from Oblivio module. The logic previously performing the
    workload is rebuilt into Inventory class.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 '''
 MIT License
 
-Copyright (c) 2018 
+Copyright (c) 2019
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -28,10 +28,8 @@ SOFTWARE.
 import os
 import sys
 import json
-import subprocess
 import argparse
 from Oblivio import *
-
 
 # Add commandline parameters when running Oblivio
 argument_parser = argparse.ArgumentParser(
@@ -48,7 +46,9 @@ help_strings = (
     'Where Oblivio will store outputfiles',
     'Email adress for the GAM G suite user',
     'True or False. False will still create a local file.',
-    'Remove the local file after upload.'
+    'Remove the local file after upload.',
+    '(int) Number of days unused for a device to be included. Default is 10.',
+    'See the output from Oblivio in the shell. No upload or file creation.'
 )
 
 # Add commandline switches
@@ -56,65 +56,42 @@ argument_parser.add_argument('-example', help = help_strings[0])
 argument_parser.add_argument('gampath', help = help_strings[1])
 argument_parser.add_argument('outpath', help = help_strings[2])
 argument_parser.add_argument('user', help = help_strings[3])
-argument_parser.add_argument('upload', help = help_strings[4])
-argument_parser.add_argument('-nofile', help = help_strings[5])
+argument_parser.add_argument('-nofile', action = 'store_true', help = help_strings[4])
+argument_parser.add_argument('-timedelta', help = help_strings[5])
+argument_parser.add_argument('-verbose', action = 'store_true', help = help_strings[6])
 
 # Parse all arguments given in to list object
 ARGS = argument_parser.parse_args()
 
-def main():
+def verify_prereq(location):
+
+    fail = None
     
     # Verify platform compatibility
-    assert(
-        'win32' in sys.platform
-    ), err_handler(exception_type = Exception, task = 'platform')
-    # Check that GAM resides in the presumed directory
-    assert(
-        os.path.isfile(GAM)
-    ), err_handler(exception_type = Exception, task = 'gam_installed')
+    if not 'win32' in sys.platform:
+        err_handler(exception_type = Exception, task = 'platform')
+        fail = True
+    
+    # Check that GAM resides in the directory
+    if os.path.isfile(f'{location}\\gam.exe') == False:
+        err_handler(exception_type = Exception, task = 'gam_installed',)
+        fail = True
+    
     # Check that oauth2.txt file with credentials exists
-    assert(
-        os.path.isfile(GAMDIR + 'oauth2.txt')
-    ), err_handler(exception_type = Exception, task = 'gam_installed')
-    # Check that Oblivio directory exists, otherwise, create it
-    if os.path.isdir(OBLIVIODIR) == False:
-        os.mkdir(OBLIVIODIR)
-    # Get user ID to use when calling GAM for file uploads to Google
-    user_id = get_user_id()
-    # Datestring instance with today's date and 10 days ago
-    dateobj = Datestring()
-    # Fetch list of active chrome devices from G Suite with GAM
-    active_crosdev = get_cros(
-        dateobj.present, dateobj.past, domain_wide = False
-    )
-    # Fetch list of all chrome devices from G Suite
-    all_crosdev = get_cros(
-        dateobj.present, dateobj.past, domain_wide = True
-    )
-    # Calculate the inactive devices by subtracting the active ones
-    if len(active_crosdev) != len(all_crosdev):
-        inactive_crosdev = compute_diff(active_crosdev, all_crosdev)
-        # Instanciate object to upload data to Google Drive
-        oblivio = InactiveDevicesCsv(
-            inactive_crosdev, OBLIVIODIR, GAM, GAMDIR, user_id, dateobj
-        )
-        # Create csv locally containing all inactive devices
-        oblivio.create_csv()
-        # Upload csv
-        oblivio.upload_csv()
-    else:
-        inactive_crosdev = None
-        print(
-            'Oblivio: No inactive devices found for the current timespan.'
-        )
+    if os.path.isfile(f'{location}\\oauth2.txt') == False:
+        err_handler(exception_type = Exception, task = 'oauthfile')
+        fail = True
 
-def get_user_id(gamdir):                                # UNFINISHED
+    if not fail:
+        return True
+
+def get_user_id(filepath):
     ''' Return the username email for the authenticated 
     GAM user, stripped of the domain name to pass on to
     GAM. This is fetched if the 'default' parameter is
     given on the USER switch when Oblivio is run. '''
     try:
-        with open(gamdir + 'oauth2.txt') as _file:
+        with open(filepath) as _file:
             _user_id = json.load(_file)
             _user_id = _user_id.get('id_token')
             _user_id = _user_id.get('email')
@@ -127,71 +104,50 @@ def get_user_id(gamdir):                                # UNFINISHED
                 _user_id[0:index - 1]
                 break
         return _user_id
-    
-def get_cros(today, then, domain_wide = False):
-    ''' Call GAM and fetch Chrome OS devices from the domain. 
-    domain_wide or not will determine if all chrome os devices are 
-    fetched or only the active ones in the given time frame. 
-    'today' and 'then' variables are date objects in string format 
-    given as a time frame for the device queries.'''
 
-    devices_arr = []
-
-    if domain_wide == True:
-        gam_command = [
-            GAM, 'print', 'cros', 'orderby',  'lastsync', 'status',
-            'fields', 'status', 'lastsync', 'serialnumber', 'OU'
-        ]
-    else:
-        gam_command = [
-            GAM, 'print', 'cros', 'query', 
-            'sync:' + str(then + '..' + today), 
-            'fields', 'status', 'lastsync', 'serialnumber', 'orderby', 
-             'lastsync', 'status', 'serialnumber', 'OU'
-        ]
-    
-    try:
-        # Call GAM and run command depending on 'domain wide' or not
-        gam_call = subprocess.run(gam_command, capture_output = True)
-        gam_output = str(gam_call)
-        
-        # Format each device in the GAM output with removed clutter
-        gam_output = gam_output.split('\\r\\n')
-    except:
-        err_handler(exception_type = RuntimeError, task = 'gam_call')
-    else:
-        # # Comprehend a list containing all devices in the GAM output
-        for i in gam_output:
-            if not 'stderr' in i and not 'print' in i:
-                devices_arr.append(i)
-        return devices_arr
-
-
-def compute_diff(active_devices, all_devices):
-    ''' Calculate the inactive devices in the given time frame.
-    List is made containing only the devices not occurring in 
-    both all and active devices. This yields the devices not used
-    in the given time frame. '''
-    inactive_devices = [
-        i for i in all_devices if not i in active_devices and not 'DEPROVISIONED' in i
-    ]
-
-    if len(inactive_devices):
-        # Delete last entry which is always empty
-        del inactive_devices[-1]
-        # Keep only last sync date and serialnumber for each index
-        for i in range(len(inactive_devices)):
-            inactive_devices[i] = inactive_devices[i].split(',')
-            inactive_devices[i] = inactive_devices[i][2:]
-
-        # Add header tag at the beginning of the list
-        _phrase = ['Last used', 'Serialnumber', 'Organizational unit']
-        
-        inactive_devices.insert(0, _phrase)
-        return inactive_devices
-    else:
-        return None
-   
 if __name__ == '__main__':
-   # main() DEBUG
-   print(ARGS.user)
+    
+    # Verify that GAM and Oauth file is found
+    ready = verify_prereq(ARGS.gampath)
+    if ready == False:
+        sys.exit()
+
+    # If commandline argument is 'default' on user parameter, fetch it
+    if ARGS.user == 'default':
+        user_id = get_user_id(f'{ARGS.gampath}\\oauth2.txt')
+
+    # If timedelda is not set by commandline argument, use default
+    if not ARGS.timedelta:
+        __delta = 10
+    else:
+        __delta = ARGS.timedelta
+
+    # Create instance of Inventory object and set properties
+    oblivio = Inventory(delta = __delta, gam_path = f'{ARGS.gampath}\\gam.exe')
+
+    # If commandline argument '-verbose' is given, print to screen
+    # and do not upload or create any files on the system.
+if ARGS.verbose:
+    print('\n','ALL DEVICES:', len(oblivio._inactive_devices), '\n')
+    for i in oblivio._all_devices:
+        print(i, end = '\n')
+
+    print('\n','ACTIVE DEVICES:', len(oblivio._active_devices), '\n')
+    for i in oblivio._active_devices:
+        print(i, end = '\n')
+    
+    print('\n','INACTIVE DEVICES:', len(oblivio._inactive_devices),'\n')
+    for i in oblivio._inactive_devices:
+        print(i, end = '\n')
+    
+    print('\n','PROVISIONED DEVICES:', len(oblivio._provisioned), '\n')
+    for i in oblivio._provisioned:
+        print(i, end = '\n')
+    
+    print('\n','DEPROVISIONED DEVICES:', len(oblivio._deprovisioned), '\n')
+    for i in oblivio._deprovisioned:
+        print(i, end = '\n')
+    
+    print('\n','DISABLED DEVICES:', len(oblivio._disabled), '\n')
+    for i in oblivio._disabled:
+        print(i, end = '\n')
