0961d6db90185c036b199e52e160e00adb9977c0
dotchetter <dotchetter@protonmail.ch>
2019-03-15 16:19:54 +0100

    Features and bugfixes:
    * Rewritten property methods
    * Create_file method writes correct headers in each worksheet
    * upload_file method written

--- Oblivio.py
+++ Oblivio.py
@@ -62,16 +62,14 @@ class Inventory(Datestring):
         # Instanciate Datestring Object
         Datestring.__init__(self, delta = int(delta))
         self._gam_path = gam_path
-        # Set properties
-        self.all_devices
-        self.active_devices
-        self.inactive_devices
-        self.provisioned
-        self.deprovisioned
-        self.disabled
 
-    @property
-    def all_devices(self):
+        __all = self.__fetch_all_devices()
+        __active = self.__fetch_active_devices()
+
+        self._all_devices = __all
+        self._active_devices = __active
+
+    def __fetch_all_devices(self):
         ''' Pass arguments to GAM and redirect stdout to 
         parse the output of devices that are recieved. 
         Get all cros devices in the domain in tuple. '''
@@ -87,10 +85,9 @@ class Inventory(Datestring):
 
         # Call method to pass arguments to GAM
         # Set the property to the returned list object as a set()
-        self._all_devices = self.init_gam(_cmd)
-            
-    @property
-    def active_devices(self):
+        return self.init_gam(_cmd)
+
+    def __fetch_active_devices(self):
         ''' Pass arguments to GAM and redirect stdout 
         to parse the output of devices that are recieved. 
         Get all cros devices in the domain in tuple. '''
@@ -108,47 +105,7 @@ class Inventory(Datestring):
 
         # Call method to pass arguments to GAM
         # Set the property to the returned list object as List
-        self._active_devices = self.init_gam(_cmd)
-
-    @property
-    def inactive_devices(self):
-        ''' Field containing a subtraction of all active
-        devices from all_devices, leaving only the inactive 
-        devices. '''
-
-        self._inactive_devices = [
-            i for i in self._all_devices if not i in self._active_devices
-        ]
-
-    @property
-    def provisioned(self):
-        ''' Field containing devices that were found to be
-        inactive and are active in the domain '''
-
-        # Comprehend list with only the provisioned "inactive devices"
-        self._provisioned = [
-            i for i in self._inactive_devices if i[0] == 'ACTIVE'
-        ]
-
-    @property
-    def deprovisioned(self):
-        ''' Field containing devices that were found to be
-        inactive and are deprovisioned in the domain '''
-
-        # Comprehend list with only the deprovisioned inactive devices
-        self._deprovisioned = [
-            i for i in self._inactive_devices if i[0] == 'DEPROVISIONED'
-        ]
-
-    @property
-    def disabled(self):
-        ''' Field containing devices that were found to be
-        inactive and are disabled in the domain '''
-
-        # Comprehend list with only the disabled inactive devices
-        self._disabled = [
-            i for i in self._inactive_devices if i[0] == 'DISABLED'
-        ]
+        return self.init_gam(_cmd)
 
     def init_gam(self, cmdlist):
         ''' Process a series of commands passed. 
@@ -179,18 +136,78 @@ class Inventory(Datestring):
                 __devicelist[index] = __devicelist[index][1:]
             return __devicelist
 
+    @property
+    def gam_path(self):
+        return self._gam_path
+    
+    @property
+    def all_devices(self):
+        return self._all_devices
+
+    @property
+    def active_devices(self):
+        return self._active_devices
+
+    @property
+    def inactive_devices(self):
+        ''' Field containing a subtraction of all active
+        devices from all_devices, leaving only the inactive 
+        devices. '''
+
+        # Comprehend list with all the inactive devices
+        self._inactive_devices = [
+            i for i in self._all_devices if not i in self._active_devices
+        ]
+        return self._inactive_devices
+
+    @property
+    def provisioned(self):
+        ''' Field containing devices that were found to be
+        inactive and are active in the domain '''
+
+        # Comprehend list with only the provisioned "inactive devices"
+        self._provisioned = [
+            i for i in self._inactive_devices if i[0] == 'ACTIVE'
+        ]
+        return self._provisioned
+
+    @property
+    def deprovisioned(self):
+        ''' Field containing devices that were found to be
+        inactive and are deprovisioned in the domain '''
+
+        # Comprehend list with only the deprovisioned inactive devices
+        self._deprovisioned = [
+            i for i in self._inactive_devices if i[0] == 'DEPROVISIONED'
+        ]
+        return self._deprovisioned
+
+    @property
+    def disabled(self):
+        ''' Field containing devices that were found to be
+        inactive and are disabled in the domain '''
+
+        # Comprehend list with only the disabled inactive devices
+        self._disabled = [
+            i for i in self._inactive_devices if i[0] == 'DISABLED'
+        ]
+        return self._disabled
+
+
+
 class Localfile():
     ''' Create an object that holds a list of inactive devices
     and format them in a CSV format. Methods for creating the CSV 
     file locally, and uploading the CSV to G Suite using GAM as 
     separate process.'''
     
-    def __init__(self, inventoryobj, outpath):
+    def __init__(self, inventoryobj, outpath, user_id):
        
         self._inventoryobj = inventoryobj
         # Filepath where to save the outputfile
         self._outpath = f'{outpath}\\Oblivio {self._inventoryobj.present}.xlsx'
-        print(self._outpath)
+        # User ID when uploading file to Google Drive with GAM
+        self._username = user_id
 
         # Assert the existence of the output directory
         if os.path.isdir(outpath) == False:
@@ -215,19 +232,23 @@ class Localfile():
 
             # Populate all inactive devices in the first sheet
             for index, string in enumerate(HEAD):
-                ws_all.write_row(0, 0, string)
-            for index, device in enumerate(self._inventoryobj._inactive_devices):
+                ws_all.write_row(0, index, string)
+            for index, device in enumerate(self._inventoryobj.inactive_devices):
                 ws_all.write_row((index + 1), 0, device)
             # Populate the 'Provisioned' sheet in the file
             for index, string in enumerate(HEAD):
-                ws_all.write_row(0, 0, string)
-            for index, device in enumerate(self._inventoryobj._provisioned):
+                ws_provisioned.write_row(0, index, string)
+            for index, device in enumerate(self._inventoryobj.provisioned):
                 ws_provisioned.write_row((index + 1), 0, device)
             # Populate the 'Deprovisioned' sheet in the file
-            for index, device in enumerate(self._inventoryobj._deprovisioned):
+            for index, string in enumerate(HEAD):
+                ws_deprovisioned.write_row(0, index, string)
+            for index, device in enumerate(self._inventoryobj.deprovisioned):
                 ws_deprovisioned.write_row((index + 1), 0, device)
             # Populate the 'Disabled' sheet in the file
-            for index, device in enumerate(self._inventoryobj._disabled):
+            for index, string in enumerate(HEAD):
+                ws_disabled.write_row(0, index, string)
+            for index, device in enumerate(self._inventoryobj.disabled):
                 ws_disabled.write_row((index + 1), 0, device)
             # Close file
             wb.close()
@@ -237,8 +258,23 @@ class Localfile():
             return True
 
     def upload_file(self):
-        pass
-
-def err_handler(msg, task):
-    ''' Handle errors on exception and stop execution '''
-    pass
+        ''' Generate list with GAM arguments to upload csv to Google Drive '''
+        _cmd = (
+            self._inventoryobj.gam_path, 'user',
+            self._username, 'add',
+            'drivefile', 'localfile', 
+            self._outpath, 'convert'
+        )
+        print('DEBUG: ', self._outpath, _cmd)
+        try:
+            # Call GAM to upload the CSV to Google 
+            _gam_call = subprocess.run(_cmd, capture_output = True)
+            if 'unauthorized_client' in str(_gam_call):        
+                print('It seems as though you have not authorized GAM to '
+                    'perform this query. Ensure that your GAM project is '
+                    'authorized with the Google Drive API.'
+                )
+        except Exception as e:
+            raise(e)
+        else:
+            return True
